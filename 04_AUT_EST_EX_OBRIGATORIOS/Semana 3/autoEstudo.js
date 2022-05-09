1baef747595ea36965672d61f9878b5911ac4d95
Leao09 <99265654+Leao09@users.noreply.github.com>
2022-05-09 15:28:31 -0300

    Entregas
    perdão esqueci de comitar e só percebi hoje

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/autoEstudo.js
@@ -0,0 +1,50 @@
+
+var calcResDiv = "Calcula"
+var g = 10
+
+function calculaTempo(v0, g){
+    var operator = "/";
+    var v0 = document.getElementById("v0").value;
+    var t = calculoT(v0, operator, g);
+    mostraOperacao_Tenp(v0, operator, g);
+    mostraResultado(t);
+
+}
+
+function calculaHmax(v0, g){
+    var operator = "/";
+    var v0 = document.getElementById("v0").value;
+    var h = calculoH(v0, operator, g);
+    console.log(v0)
+    mostraOperacao_Hmax(v0, operator, g);
+    mostraResultado(h);
+}
+
+
+function calculoT(v0, operator, g){
+    if(operator == "/"){
+        t = v0/g;
+       
+    }
+    
+    return t;
+};                                      
+function calculoH(v0, operator, g){
+    if(operator == "/"){
+        h = (v0*v0)/(2*g);
+    }
+    return h;
+}
+
+function mostraOperacao_Tenp(v0, operator, g){
+    document.getElementById(calcResDiv).innerHTML += `<br /> ${v0} ${operator} ${g} = `;
+}
+function mostraOperacao_Hmax(v0, operator, g){
+    document.getElementById(calcResDiv).innerHTML += `<br /> ${v0*v0} ${operator} ${2*g} = `;
+}
+
+
+
+function mostraResultado(res){
+    document.getElementById(calcResDiv).innerHTML += res;
+}
